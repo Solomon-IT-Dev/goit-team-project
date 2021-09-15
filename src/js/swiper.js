80053e8a6de9546d346a81c5612affa08a86c449
o-ishchenko <olya.galaychuk@gmail.com>
2021-09-15 18:34:20 +0300

    Добавляет автопрокрутку и стрелки

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,17 @@
 new Swiper('.reviews-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        stopOnLastSlide: false,
+        disbleOnInteraction: true
     }
 });
